cdc4865c9eb553b65c9aa61bf77eb3e70f1a121f
niazmurshedsiam <niazmurshed16@gmail.com>
2023-04-01 01:27:45 +0600

    Get all data from database

---
+++ MVCRepositoryEntity/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using MVCRepositoryEntity.Interface;
+using MVCRepositoryEntity.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRepositoryEntity.Controllers
+{
+    public class StudentController : Controller
+    {
+        IStudentManager _iStudent = new StudentManager();
+        // GET: Student
+        public ActionResult Index()
+        {
+            var students = _iStudent.GetAll();
+            return View(students);
+        }
+    }
+}
